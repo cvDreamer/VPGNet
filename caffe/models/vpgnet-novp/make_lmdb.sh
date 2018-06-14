01d3fa55563c81a98c6ae24e30263b28316e1616
cvDreamer <foreversean@gmail.com>
2018-06-13 23:47:49 -0500

    Update make_lmdb.sh

--- caffe/models/vpgnet-novp/make_lmdb.sh
+++ caffe/models/vpgnet-novp/make_lmdb.sh
@@ -1,8 +1,8 @@
 # Declare $PATH_TO_DATASET_DIR and $PATH_TO_DATASET_LIST
 
-../../build/tools/convert_driving_data VPGNet/caltech-lanes-dataset /workspace/VPGNet/caltech-lanes-dataset/washington1.txt LMDB_train
+../../build/tools/convert_driving_data /workspace/VPGNet/caltech-lanes-dataset /workspace/VPGNet/caltech-lanes-dataset/washington1.txt LMDB_train
 ../../build/tools/compute_driving_mean LMDB_train ./driving_mean_train.binaryproto lmdb
 
-../../build/tools/convert_driving_data VPGNet/caltech-lanes-dataset /workspace/VPGNet/caltech-lanes-dataset/washington1.txt LMDB_test
+../../build/tools/convert_driving_data /workspace/VPGNet/caltech-lanes-dataset /workspace/VPGNet/caltech-lanes-dataset/washington1.txt LMDB_test
 
 
