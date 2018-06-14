8839ed65bc6bedf07f2d332892b4b1b4f419f649
cvDreamer <foreversean@gmail.com>
2018-06-13 23:53:10 -0500

    Explicitly specify paths
    Explicitly specify path values for $PATH_TO_DATASET_DIR and $PATH_TO_DATASET_LIST to enable testing a docker build, will change back later to original value

--- caffe/models/vpgnet-novp/make_lmdb.sh
+++ caffe/models/vpgnet-novp/make_lmdb.sh
@@ -1,4 +1,7 @@
 # Declare $PATH_TO_DATASET_DIR and $PATH_TO_DATASET_LIST
+# Explictly set value of $PATH_TO_DATASET_DIR = /workspace/VPGNet/caltech-lanes-dataset
+# and $PATH_TO_DATASET_LIST = /workspace/VPGNet/caltech-lanes-dataset/washington1.txt to enable testing a docker build
+# Will change back later
 
 ../../build/tools/convert_driving_data /workspace/VPGNet/caltech-lanes-dataset /workspace/VPGNet/caltech-lanes-dataset/washington1.txt LMDB_train
 ../../build/tools/compute_driving_mean LMDB_train ./driving_mean_train.binaryproto lmdb
